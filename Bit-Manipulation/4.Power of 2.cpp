cc1776539db0c94f86cb1c8f3415ec4524287159
Shriyanshi Sharma <shriyanshi.19b131153@abes.ac.in>
2022-06-22 00:49:29 +0530

    BITMANIPULATION

---
+++ Bit-Manipulation/4.Power of 2.cpp
@@ -0,0 +1,34 @@
+// Power of 2 
+/*
+Given a non-negative integer N. The task is to check if N is a power of 2. More formally, check if N can be expressed as 2x for some x.
+
+Example 1:
+Input: N = 1
+Output: YES
+Explanation:1 is equal to 2 
+raised to 0 (20 = 1).
+
+Example 2:
+Input: N = 98
+Output: NO
+Explanation: 98 cannot be obtained
+by any power of 2.
+
+Your Task:Your task is to complete the function isPowerofTwo() which takes n as a parameter and returns true or false by checking if the given number can be represented as a power of two or not.
+
+Expected Time Complexity:O(log N).
+Expected Auxiliary Space:O(1).
+*/
+class Solution{
+    public:
+    bool isPowerofTwo(long long n){
+        int c =0;
+        while(n){
+            if(n&1) c++;
+            n = n >> 1 ;
+        }
+        if(c == 1 ) return true;
+        return false;
+        
+    }
+};
